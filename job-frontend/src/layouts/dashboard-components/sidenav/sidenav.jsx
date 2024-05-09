ce36159bdbc9428927d63879f7515bb5f8c410c8
Rohit Sahu <irohitsahu97@gmail.com>
2024-05-09 07:39:36 +0530

    sidenav bar fixes

--- job-frontend/src/layouts/dashboard-components/sidenav/sidenav.jsx
+++ job-frontend/src/layouts/dashboard-components/sidenav/sidenav.jsx
@@ -7,29 +7,26 @@ import { useLocation, NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // @mui material components
-import {
-  List,
-  Divider,
-  Link,
-  Icon,
-  Box,
-  Typography,
-  Button,
-} from "@mui/material";
+import { List, Divider, Link, Box, Typography, ListItem } from "@mui/material";
 
-// Material Dashboard 2 React example components
-import SidenavCollapse from "./SidenavLayout";
+import CloseIcon from "@mui/icons-material/Close";
 
 // Custom styles for the Sidenav
-import SidenavRoot from "./sidenavRoot";
-import sidenavLogoLabel from "./styles/sidenav";
+import {
+  CollapseIcon,
+  CollapseIconBox,
+  CollapseItem,
+  CollapseText,
+  SideNavbarDrawer,
+  SideNavbarLogoLabel,
+} from "./sidenav-styles";
 
 // Material Dashboard 2 React context
 import { useMaterialUIController, setMiniSidenav } from "context";
 
-function Sidenav({ color, brand, brandName, routes, ...rest }) {
+function Sidenav({ brand, brandName, routes, ...rest }) {
   const [controller, dispatch] = useMaterialUIController();
-  const { miniSidenav, darkMode, sidenavColor } = controller;
+  const { miniSidenav, darkMode } = controller;
   const location = useLocation();
   const collapseName = location.pathname.replace("/", "");
 
@@ -37,27 +34,18 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
 
   const closeSidenav = () => setMiniSidenav(dispatch, true);
 
+  // it is controling the sidenav close when screen is small
   useEffect(() => {
-    // A function that sets the mini state of the sidenav.
     function handleMiniSidenav() {
       setMiniSidenav(dispatch, window.innerWidth < 1200);
     }
-
-    /** 
-     The event listener that's calling the handleMiniSidenav function when resizing the window.
-    */
     window.addEventListener("resize", handleMiniSidenav);
-
-    // Call the handleMiniSidenav function to set the state with the initial value.
     handleMiniSidenav();
-
-    // Remove event listener on cleanup
     return () => window.removeEventListener("resize", handleMiniSidenav);
   }, [dispatch, location]);
 
-  // Render all the routes from the routes.js (All the visible items on the Sidenav)
   const renderRoutes = routes.map(
-    ({ type, name, icon, title, noCollapse, key, href, route }) => {
+    ({ type, name, icon, title, key, href, route }) => {
       let returnValue;
 
       if (type === "collapse") {
@@ -69,20 +57,59 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
             rel="noreferrer"
             sx={{ textDecoration: "none" }}
           >
-            <SidenavCollapse
-              name={name}
-              icon={icon}
-              active={key === collapseName}
-              noCollapse={noCollapse}
-            />
+            <ListItem component="li">
+              <CollapseItem ownerState={key === collapseName}>
+                <CollapseIconBox
+                  ownerState={{ darkMode, active: key === collapseName }}
+                >
+                  {typeof icon === "string" ? (
+                    <CollapseIcon
+                      ownerState={{ darkMode, active: key === collapseName }}
+                    >
+                      {icon}
+                    </CollapseIcon>
+                  ) : (
+                    icon
+                  )}
+                </CollapseIconBox>
+
+                <CollapseText
+                  primary={name}
+                  ownerState={{
+                    miniSidenav,
+                    active: key === collapseName,
+                  }}
+                />
+              </CollapseItem>
+            </ListItem>
           </Link>
         ) : (
           <NavLink key={key} to={route}>
-            <SidenavCollapse
-              name={name}
-              icon={icon}
-              active={key === collapseName}
-            />
+            <ListItem component="li">
+              <CollapseItem ownerState={key === collapseName}>
+                <CollapseIconBox
+                  ownerState={{ darkMode, active: key === collapseName }}
+                >
+                  {typeof icon === "string" ? (
+                    <CollapseIcon
+                      ownerState={{ darkMode, active: key === collapseName }}
+                    >
+                      {icon}
+                    </CollapseIcon>
+                  ) : (
+                    icon
+                  )}
+                </CollapseIconBox>
+
+                <CollapseText
+                  primary={name}
+                  ownerState={{
+                    miniSidenav,
+                    active: key === collapseName,
+                  }}
+                />
+              </CollapseItem>
+            </ListItem>
           </NavLink>
         );
       } else if (type === "title") {
@@ -103,7 +130,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           </Typography>
         );
       } else if (type === "divider") {
-        returnValue = <Divider key={key} light={true} />;
+        returnValue = <Divider key={key} />;
       }
 
       return returnValue;
@@ -111,7 +138,11 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
   );
 
   return (
-    <SidenavRoot {...rest} variant="permanent" ownerState={{ darkMode }}>
+    <SideNavbarDrawer
+      {...rest}
+      variant="permanent"
+      ownerState={{ miniSidenav, darkMode }}
+    >
       <Box pt={3} pb={1} px={4} textAlign="center">
         <Box
           display={{ xs: "block", xl: "none" }}
@@ -122,45 +153,33 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           onClick={closeSidenav}
           sx={{ cursor: "pointer" }}
         >
-          <Typography variant="h6" color="secondary">
-            <Icon sx={{ fontWeight: "bold" }}>close</Icon>
-          </Typography>
+          <CloseIcon />
         </Box>
-        <Box component={NavLink} to="/" display="flex" alignItems="center">
+        <Box
+          component={NavLink}
+          to="/"
+          display="flex"
+          alignItems="center"
+          sx={{ textDecoration: "none", color: "primary.main" }}
+        >
           {brand && (
             <Box component="img" src={brand} alt="Brand" width="2rem" />
           )}
-          <Box
-            width={!brandName && "100%"}
-            sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
-          >
+          <SideNavbarLogoLabel {...rest} ownerState={{ darkMode }}>
             <Typography
-              component="h6"
+              component="h1"
               variant="button"
               fontWeight="medium"
               color={textColor}
             >
               {brandName}
             </Typography>
-          </Box>
+          </SideNavbarLogoLabel>
         </Box>
       </Box>
-      <Divider light={true} />
+      <Divider variant="middle" sx={{ backgroundColor: "secondary.main" }} />
       <List>{renderRoutes}</List>
-      <Box p={2} mt="auto">
-        <Button
-          component="a"
-          href="https://www.creative-tim.com/product/material-dashboard-pro-react"
-          target="_blank"
-          rel="noreferrer"
-          variant="gradient"
-          color={sidenavColor}
-          fullWidth
-        >
-          upgrade to pro
-        </Button>
-      </Box>
-    </SidenavRoot>
+    </SideNavbarDrawer>
   );
 }
 
